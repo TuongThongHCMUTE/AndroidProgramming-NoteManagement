b0d4981dd692bffdda19979007a115fb34b6a9c4
stark3105 <bachthong.dinh@gmail.com>
2021-05-07 22:14:37 +0700

    Update HomeFragment.java

--- NoteManagement/app/src/main/java/com/example/notemanagement/ui/home/HomeFragment.java
+++ NoteManagement/app/src/main/java/com/example/notemanagement/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.notemanagement.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,8 @@ import com.example.notemanagement.database.StatusDatabase;
 import com.example.notemanagement.entity.Note;
 import com.example.notemanagement.entity.Status;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
@@ -53,5 +56,48 @@ public class HomeFragment extends Fragment {
 
         color = new  int[statuses.size()];
         countColorID = 0;
+
+        for (Status i:statuses){
+            temp = i.getName();
+            checkStatusName = checkStatusName(temp);
+            setColorPieChart(countColorID++, checkStatusName);
+            count = NoteDatabase.getInstance(getContext()).NoteDAO().countNotesOfUserByStatusID(Common.userId, temp);
+            entries.add(new PieEntry(count, temp));
+        }
+
+
+        PieDataSet dataset = new PieDataSet(entries, "test");
+
+        dataset.setSliceSpace(3f);
+        dataset.setSelectionShift(5f);
+        dataset.setColors(color);
+
+        PieData data= new PieData((dataset));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.WHITE);
+
+        pieChart.setData(data);
+    }
+
+    private void setColorPieChart(int i, int checkStatusName) {
+        if(checkStatusName == wrong)
+            return;
+        if(checkStatusName == done)
+            color[i] = Color.rgb(0,0,255);
+        if(checkStatusName == processing)
+            color[i] = Color.rgb(153,153,153);
+        if(checkStatusName == pending)
+            color[i] = Color.rgb(255,0,0);
+    }
+
+
+    public int checkStatusName(String statusName){
+        if(statusName.equals("Done"))
+            return done;
+        if(statusName.equals("Processing"))
+            return processing;
+        if(statusName.equals("Pending"))
+            return pending;
+        return wrong;
     }
 }
